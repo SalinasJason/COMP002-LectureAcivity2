75cfaee846f4b45e50e4ec2ca33a91e4745278ec
Jason <jasonsalinas2@my.whccd.edu>
2024-10-23 19:05:59 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// this is a comment //
+console.log("Hello World")
+
+console.log(8 * null) //result: 0
+
+console.log("5" - 1) //result: 4
+
+console.log("5" + 1) //result: 51
+
+console.log("five" * 2) //result: NaN
+
+console.log(false == 0) //result: true 
+
+console.log(5 > 2 && 6 > 3) //result: true 
+
+console.log(false == false ? "equal" : "not equal") //result:equal 
+
+console.log(true || false) //result: true 
