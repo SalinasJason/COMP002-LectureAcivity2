6453e17919c440cb7224a774e9249a0239916897
Jason <jasonsalinas2@my.whccd.edu>
2024-10-23 20:45:20 -0700

    added conditional-code.js

---
+++ conditional-code.js
@@ -0,0 +1,14 @@
+for(let i = 0; i <= 10; i ++) {
+    console.log(i);
+}
+
+for(let i = 10; i >= 0; i --) {
+    console.log(i);
+}
+
+
+let outputVariable = ""
+for (let i = 0; i <= 10; i++){
+    outputVariable += "!";
+    console.log(outputVariable);
+}
